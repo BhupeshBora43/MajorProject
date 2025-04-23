11eb86722d5f0108db96d379718e71e5e5141463
BhupeshBora43 <bhupeshbora2004@gmail.com>
2025-04-23 21:58:48 +0530

    added origin

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-    origin:[
-        "http://localhost:5173",
-        "https://batchit-backend.onrender.com"
-      ],
+    origin: ["http://localhost:5173",
+        "https://batchit-frontend.onrender.com"
+    ],
     credentials: true
 }));
 
@@ -28,10 +27,9 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: [
-            "http://localhost:5173",
-            "https://batchit-backend.onrender.com"
-          ],
+        origin: ["http://localhost:5173",
+            "https://batchit-frontend.onrender.com"
+        ],
         methods: ["GET", "POST"],
         credentials: true
     }
