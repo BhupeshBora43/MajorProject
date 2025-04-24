c8905492575053c8f58b5851db081016b98a003f
BhupeshBora43 <bhupeshbora2004@gmail.com>
2025-04-24 15:35:52 +0530

    corrected

--- frontend/src/Helper/axiosInstance.js
+++ frontend/src/Helper/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://batchit-backend.onrender.com",
     withCredentials: true,
 });
 
